a040e55a27881e6850d4af2a417ade05fbbf7e02
Ruslan Khaydarpashich <h.ruslan2000@yandex.ru>
2024-10-29 21:01:17 +0300

    Create output file with results

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "src/linear.hpp"
 #include "src/openmp.hpp"
 #include <chrono>
+#include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <omp.h>
@@ -11,12 +12,16 @@ int main() {
 
   int maxIterations = 1e5;
   double tolerance = 1e-6;
+
   for (auto &[N, M] : NMs) {
     // Example matrices (a, b, F) initialized with some values
     std::vector<std::vector<double>> a(M, std::vector<double>(N - 1, 0.0));
     std::vector<std::vector<double>> b(M - 1, std::vector<double>(N, 0.0));
     std::vector<std::vector<double>> F(M - 1, std::vector<double>(N - 1, 0.0));
     std::vector<std::vector<double>> W(M + 1, std::vector<double>(N + 1, 0.0));
+
+    std::ofstream solutionFile("linear_" + std::to_string(M) + '_' +
+                               std::to_string(N) + ".txt");
 
     auto begin = std::chrono::high_resolution_clock::now();
     linear::computeA(a);
@@ -27,8 +32,17 @@ int main() {
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration =
         std::chrono::duration_cast<std::chrono::microseconds>(stop - begin);
-    std::cout << "Solution (" << N << "," << M << ")"
-              << " Linear took " << duration.count() << " microsecs:\n";
+    solutionFile << "Solution (" << N << "," << M << ")"
+                 << " Linear took " << duration.count() << " microsecs:\n\n";
+    // Output the result
+    for (int i = 0; i <= M; i++) {
+      for (int j = 0; j <= N; j++) {
+        solutionFile << std::fixed << std::setprecision(4) << W[i][j] << " ";
+      }
+      solutionFile << std::endl;
+    }
+    solutionFile.close();
+
     // Back to default values
     for (int i = 0; i < M - 1; ++i) {
       std::fill(b[i].begin(), b[i].end(), 0.0);
@@ -42,14 +56,6 @@ int main() {
     }
   }
 
-  // // Output the result
-  // for (int i = 0; i <= M; i++) {
-  //     for (int j = 0; j <= N; j++) {
-  //     std::cout << std::fixed << std::setprecision(4) << W[i][j] << " ";
-  //     }
-  //     std::cout << std::endl;
-  // }   std::cout << std::endl;
-
   // // printABF(a, b, F);
   // // std::cout << "\n\n\n";
 
@@ -61,19 +67,37 @@ int main() {
     std::vector<std::vector<double>> F(M - 1, std::vector<double>(N - 1, 0.0));
     std::vector<std::vector<double>> W(M + 1, std::vector<double>(N + 1, 0.0));
 
+    std::ofstream solutionFile("openmp_" + std::to_string(M) + '_' +
+                               std::to_string(N) + "_TN" +
+                               std::to_string(threadNum) + ".txt");
+
     auto start = omp_get_wtime();
     computeJointABF(a, b, F, threadNum);
     calculateW(a, b, F, W, maxIterations, tolerance, threadNum);
     auto end = omp_get_wtime();
     // Output the result
-    std::cout << "Solution (" << N << "," << M << ")"
-              << " OpenMP took " << (end - start) * 1e6 << " microsecs:\n";
-    // for (int i = 0; i <= M; i++) {
-    //     for (int j = 0; j <= N; j++) {
-    //     std::cout << std::fixed << std::setprecision(4) << W[i][j] << " ";
-    //     }
-    //     std::cout << std::endl;
-    // }
+    solutionFile << std::fixed << "Solution (" << N << "," << M << ")"
+                 << " OpenMP took (" << threadNum << " threads) "
+                 << static_cast<uint>((end - start) * 1e6) << " microsecs:\n\n";
+    for (int i = 0; i <= M; i++) {
+      for (int j = 0; j <= N; j++) {
+        solutionFile << std::fixed << std::setprecision(4) << W[i][j] << " ";
+      }
+      solutionFile << std::endl;
+    }
+    solutionFile.close();
+
+    // Back to default values
+    for (int i = 0; i < M - 1; ++i) {
+      std::fill(b[i].begin(), b[i].end(), 0.0);
+      std::fill(F[i].begin(), F[i].end(), 0.0);
+    }
+    for (int i = 0; i < M; ++i) {
+      std::fill(a[i].begin(), a[i].end(), 0.0);
+    }
+    for (int i = 0; i < M + 1; ++i) {
+      std::fill(W[i].begin(), W[i].end(), 0.0);
+    }
   }
   return 0;
 }
