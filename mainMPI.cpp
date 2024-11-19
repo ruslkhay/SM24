0b18581c4b82405750d6e96862878dc4f15e6237
Ruslan Khaydarpashich <h.ruslan2000@yandex.ru>
2024-11-19 20:48:26 +0300

    Build communication between 4 processes

--- mainMPI.cpp
+++ mainMPI.cpp
@@ -17,11 +17,42 @@ int *prepareSubGrid(std::array<std::array<int, M>, N> &grid, int x0, int xM,
   int *tmpBuf = new int[count];
   for (int i = y0; i < yN; ++i) {
     for (int j = x0; j < xM; ++j) {
-      int a = grid[i][j];
-      tmpBuf[(i - y0) * (xM - x0) + j] = a;
+      tmpBuf[(i - y0) * (xM - x0) + (j - x0)] = grid[i][j];
     }
   }
   return tmpBuf;
+}
+
+template <std::size_t M, std::size_t N>
+void debugSendPrint(const std::array<std::array<int, M>, N> &grid,
+                    const int currRank, const int nextRank, int x0, int xM,
+                    int y0, int yN) {
+  auto message =
+      std::string(">> Process " + std::to_string(currRank) + " doing:\n");
+  switch (currRank) {
+  case 0:
+    message += "  Top Left Sub-grid:\n";
+    break;
+  case 1:
+    message += "  Bottom Left Sub-grid:\n";
+    break;
+  case 2:
+    message += "  Bottom Right Sub-grid:\n";
+    break;
+  case 3:
+    message += "  Top Right Sub-grid:\n";
+    break;
+  }
+  for (int i = y0; i < yN; ++i) {
+    for (int j = x0; j < xM; ++j) {
+      message += std::to_string(grid[i][j]);
+      message += "|";
+    }
+    message += '\n';
+  }
+  message += "  And sending it to process " + std::to_string(nextRank) + "\n";
+  message += "----------\n";
+  std::cout << message << std::endl;
 }
 
 const int M = 5, N = 5;
@@ -35,13 +66,7 @@ std::array<std::array<int, M>, N> grid = {1, 1, 1, 1, 9, 2, 2, 2, 2, 8, 3, 3, 3,
 
 int main(int argc, char **argv) {
   int rank, size;
-
-  const int topLeftRows = 2, topLeftCols = 3;
   const int xMiddle = M / 2 + 1, yMiddle = N / 2 + 1;
-  const int topRightRows = 2, topRightCols = 2;
-  const int bottomLeftRows = 3, bottomLeftCols = 3;
-  const int bottomRightRows = 3; //, bottomRightCols = 2;
-
   // Initialize the MPI environment
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -59,72 +84,41 @@ int main(int argc, char **argv) {
     }
     std::cout << "\n";
   }
-
-  // Update the grid with data specific for each rank
-  int sub_grid[3][3]; // Maximum size for receiving
-
-  // Create buffers for each process based on sub-grid sizes
-  // int third[2][2];  // Process 2
-  // int fourth[3][2]; // Process 3
+  MPI_Barrier(MPI_COMM_WORLD);
 
   // Distribute the data accordingly
   if (rank == 0) {
     int nextRank = 1;
-    printf(">> Process %d doing:\n", rank);
-    // Print Top Left Sub-grid
-    std::cout << "  Top Left Sub-grid:\n";
-    for (int i = 0; i < N - yMiddle; ++i) {
-      for (int j = 0; j < xMiddle; ++j) {
-        std::cout << grid[i][j] << "|";
-      }
-      std::cout << "\n";
-    }
-    printf("  And sending it to process %d\n", nextRank);
-    // Process 0: top-left 2x3 grid
-    auto tmpBuf = prepareSubGrid(grid, 0, xMiddle, 0, N - yMiddle);
-    int count = xMiddle * (N - yMiddle);
+    int x0 = 0, xM = xMiddle, y0 = 0, yN = N - yMiddle;
+    debugSendPrint(grid, rank, nextRank, x0, xM, y0, yN);
+    auto tmpBuf = prepareSubGrid(grid, x0, xM, y0, yN);
+    int count = (xM - x0) * (yN - y0);
     MPI_Send(&tmpBuf[0], count, MPI_INT, nextRank, 0, MPI_COMM_WORLD);
     delete[] tmpBuf;
   } else if (rank == 1) {
     int nextRank = 0;
-    printf(">> Process %d doing:\n", rank);
-    // Print Bottom Left Sub-grid
-    std::cout << "Bottom Left Sub-grid:\n";
-    for (int i = topLeftRows; i < topLeftRows + bottomLeftRows; ++i) {
-      for (int j = 0; j < bottomLeftCols; ++j) {
-        std::cout << grid[i][j] << "|";
-      }
-      std::cout << "\n";
-    }
-    // Process 1: bottom-left 3x3 grid
-    auto tmpBuf = prepareSubGrid(grid, 0, xMiddle, N - yMiddle, N);
-    int count = xMiddle * yMiddle;
+    int x0 = 0, xM = xMiddle, y0 = N - yMiddle, yN = N;
+    debugSendPrint(grid, rank, nextRank, x0, xM, y0, yN);
+    auto tmpBuf = prepareSubGrid(grid, x0, xM, y0, yN);
+    int count = (xM - x0) * (yN - y0);
     MPI_Send(&tmpBuf[0], count, MPI_INT, nextRank, 0, MPI_COMM_WORLD);
     delete[] tmpBuf;
   } else if (rank == 2) {
-    // printf("On process %d; processing ", rank);
-    // Print Bottom Right Sub-grid
-    std::cout << "Bottom Right Sub-grid:\n";
-    for (int i = topLeftRows; i < topLeftRows + bottomRightRows; ++i) {
-      for (int j = topLeftCols; j < topLeftCols + topRightCols; ++j) {
-        std::cout << grid[i][j] << "|";
-      }
-      std::cout << "\n";
-    }
-    // Process 2: top-right 2x2 grid
-    MPI_Send(&grid[0][3], 2 * 2, MPI_INT, 2, 0, MPI_COMM_WORLD);
+    int nextRank = 2;
+    int x0 = xMiddle, xM = M, y0 = N - yMiddle, yN = N;
+    debugSendPrint(grid, rank, nextRank, x0, xM, y0, yN);
+    auto tmpBuf = prepareSubGrid(grid, x0, xM, y0, yN);
+    int count = (xM - x0) * (yN - y0);
+    MPI_Send(&tmpBuf[0], count, MPI_INT, nextRank, 0, MPI_COMM_WORLD);
+    delete[] tmpBuf;
   } else if (rank == 3) {
-    // printf("On process %d; processing ", rank);
-    // Print Top Right Sub-grid
-    std::cout << "Top Right Sub-grid:\n";
-    for (int i = 0; i < topRightRows; ++i) {
-      for (int j = topLeftCols; j < topLeftCols + topRightCols; ++j) {
-        std::cout << grid[i][j] << "|";
-      }
-      std::cout << "\n";
-    }
-    // Process 3: bottom-right 3x2 grid
-    MPI_Send(&grid[3][0], 3 * 2, MPI_INT, 3, 0, MPI_COMM_WORLD);
+    int nextRank = 3;
+    int x0 = xMiddle, xM = M, y0 = 0, yN = N - yMiddle;
+    debugSendPrint(grid, rank, nextRank, xMiddle, M, 0, N - yMiddle);
+    auto tmpBuf = prepareSubGrid(grid, xMiddle, M, 0, N - yMiddle);
+    int count = (xM - x0) * (yN - y0);
+    MPI_Send(&tmpBuf[0], count, MPI_INT, nextRank, 0, MPI_COMM_WORLD);
+    delete[] tmpBuf;
   }
 
   // Now each process should receive its respective sub-grid
@@ -154,19 +148,32 @@ int main(int argc, char **argv) {
       printf("\n");
     }
     std::cout << "----------\n";
-    // printf("Process %d received:\n", rank);
-    // for (int i = 0; i < 3; i++) {
-    //   for (int j = 0; j < 3; j++) {
-    //     printf("%4d", second[i][j]);
-    //   }
-    //   printf("\n");
-    // }
   } else if (rank == 2) {
-    MPI_Recv(&sub_grid, 2 * 2, MPI_INT, 2, 0, MPI_COMM_WORLD,
+    int prevRank = 2;
+    int third[3][2];
+    MPI_Recv(&third, 3 * 2, MPI_INT, prevRank, 0, MPI_COMM_WORLD,
              MPI_STATUS_IGNORE);
+    printf("<< Process %d received:\n", rank);
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 2; j++) {
+        printf("%4d", third[i][j]);
+      }
+      printf("\n");
+    }
+    std::cout << "----------\n";
   } else if (rank == 3) {
-    MPI_Recv(&sub_grid, 3 * 2, MPI_INT, 3, 0, MPI_COMM_WORLD,
+    int prevRank = 3;
+    int fourth[2][2];
+    MPI_Recv(&fourth, 2 * 2, MPI_INT, prevRank, 0, MPI_COMM_WORLD,
              MPI_STATUS_IGNORE);
+    printf("<< Process %d received:\n", rank);
+    for (int i = 0; i < 2; i++) {
+      for (int j = 0; j < 2; j++) {
+        printf("%4d", fourth[i][j]);
+      }
+      printf("\n");
+    }
+    std::cout << "----------\n";
   }
   // Finalize the MPI environment
   MPI_Finalize();
