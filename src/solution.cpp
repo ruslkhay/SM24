db90867f1b6ee6e1aa3596388602aa5bab44165d
Ruslan Khaydarpashich <h.ruslan2000@yandex.ru>
2024-11-21 02:12:43 +0300

    Draft of solution class and refactor

--- src/solution.cpp
+++ src/solution.cpp
@@ -1,39 +1,82 @@
+#include <filesystem>
+#include <fstream>
 #include <solution.hpp>
-
-Grid::Grid(int M, int N, double h1, double h2) {
-  _M = M;
-  _N = N;
-  _h1 = h1;
-  _h2 = h2;
-  _nodes.assign(M, std::vector<double>(N, 0));
-}
 
 Grid::Grid(int M, int N) {
   _M = M;
   _N = N;
-  _nodes.assign(M, std::vector<double>(N, 0));
+  _h1 =
+      3.0 / M; // 3.0 is a number specific for my task (size of outer rectangle)
+  _h2 = 3.0 / N;
+  _nodes.assign(M, line_t(N, 0));
 }
 
-Grid::Grid(const std::vector<std::vector<double>> &grid) {
+Grid::Grid(const matrix_t &grid) {
   _nodes = grid;
   _M = grid.size();
   _N = grid[0].size();
 }
 
-// class Solution
-// {
-// private:
-//   int maxIterations = 1e5;
-//   double tolerance = 1e-6;
-// public:
-//     Solution(/* args */);
-//     ~Solution();
-// };
+class Solution : public Grid {
+  using time_t = std::chrono::microseconds;
 
-// Solution::Solution(/* args */)
-// {
-// }
+private:
+  std::filesystem::path _dirPath;
+  int _maxIterations;
+  time_t _execTime = time_t::duration::zero();
+  double _tolerance;
+  void CreateOutputDir(std::string buildDir = ".",
+                       std::string outputDirName = "output");
 
-// Solution::~Solution()
-// {
-// }
+public:
+  Solution(int M, int N, int maxIterations, double tolerance);
+  void SaveToFile(std::string fileName);
+  void Find();
+};
+
+Solution::Solution(int M, int N, int maxIterations, double tolerance)
+    : Grid(M + 1, N + 1) // +1 because if M = 3 => 4 nodes on X axis
+{};
+
+void Solution::Find() {
+  auto start = std::chrono::high_resolution_clock::now();
+  /* Calculations here */
+  auto stop = std::chrono::high_resolution_clock::now();
+  _execTime += std::chrono::duration_cast<time_t>(stop - start);
+};
+
+void Solution::CreateOutputDir(std::string buildDir,
+                               std::string outputDirName) {
+  const std::string buildFolder = buildDir;
+  const std::string dirName = outputDirName;
+  _dirPath = std::filesystem::path(buildFolder) / dirName;
+  // Check if the directory already exists
+  if (!std::filesystem::exists(_dirPath)) {
+    // Create the directory
+    if (std::filesystem::create_directory(_dirPath)) {
+      std::cout << "Reports are stored in: " << _dirPath << std::endl;
+    } else {
+      std::cerr << "Failed to create directory: " << _dirPath << std::endl;
+    }
+  } else {
+    std::cout << "Reports are stored in: " << _dirPath << std::endl;
+  }
+};
+
+void Solution::SaveToFile(std::string fileName) {
+  CreateOutputDir();
+  fileName =
+      fileName + "_" + std::to_string(_M) + '_' + std::to_string(_N) + ".txt";
+
+  std::ofstream solutionFile(_dirPath / fileName);
+  solutionFile << "Solution (" << _N << "," << _M << ")"
+               << " Linear took " << _execTime.count() << " microsecs:\n\n";
+  // Output the result
+  for (int i = 0; i <= _M; i++) {
+    for (int j = 0; j <= _N; j++) {
+      solutionFile << std::fixed << std::setprecision(4) << _nodes[i][j] << " ";
+    }
+    solutionFile << std::endl;
+  }
+  solutionFile.close();
+};
--- src/solution.hpp
+++ src/solution.hpp
@@ -3,62 +3,64 @@
 #include <vector>
 
 class Grid {
-private:
+  using line_t = std::vector<double>;
+  using matrix_t = std::vector<line_t>;
+
+protected:
   int _M;
   int _N;
   double _h1; // horizontal step
   double _h2; // vertical step
-  std::vector<std::vector<double>> _nodes;
+  matrix_t _nodes;
 
 public:
   Grid(int M, int N);
-  Grid(int M, int N, double h1, double h2);
-  Grid(const std::vector<std::vector<double>> &grid);
+  Grid(const matrix_t &grid);
 
-  std::vector<double> GetColumn(int m);
-  std::vector<double> GetRow(int n);
+  line_t GetColumn(int m);
+  line_t GetRow(int n);
 
-  std::vector<double> GetLeftBoarder() { return GetRow(0); };
-  std::vector<double> GetRightBoarder() { return GetRow(_N - 1); };
-  std::vector<double> GetTopBoarder() { return GetColumn(0); };
-  std::vector<double> GetBottomBoarder() { return GetColumn(_M - 1); };
+  line_t GetLeftBoarder() { return GetRow(0); };
+  line_t GetRightBoarder() { return GetRow(_N - 1); };
+  line_t GetTopBoarder() { return GetColumn(0); };
+  line_t GetBottomBoarder() { return GetColumn(_M - 1); };
 
-  inline std::vector<std::vector<double>> GetNodes() { return _nodes; }
+  inline matrix_t GetNodes() { return _nodes; }
   inline int GetM() { return _M; }
   inline int GetN() { return _N; }
 
-  void SetLeftBoarder(const std::vector<double> &newBoarder);
-  void SetRightBoarder(const std::vector<double> &newBoarder);
-  void SetTopBoarder(const std::vector<double> &newBoarder);
-  void SetBottomBoarder(const std::vector<double> &newBoarder);
+  void SetLeftBoarder(const line_t &newBoarder);
+  void SetRightBoarder(const line_t &newBoarder);
+  void SetTopBoarder(const line_t &newBoarder);
+  void SetBottomBoarder(const line_t &newBoarder);
 
   void Print();
 };
 
-inline void Grid::SetTopBoarder(const std::vector<double> &newBoarder) {
+inline void Grid::SetTopBoarder(const line_t &newBoarder) {
   _nodes[0] = newBoarder;
 }
 
-inline void Grid::SetBottomBoarder(const std::vector<double> &newBoarder) {
+inline void Grid::SetBottomBoarder(const line_t &newBoarder) {
   _nodes[_M - 1] = newBoarder;
 }
 
-inline void Grid::SetLeftBoarder(const std::vector<double> &newBoarder) {
+inline void Grid::SetLeftBoarder(const line_t &newBoarder) {
   for (int i = 0; i < _M; ++i) {
     _nodes[i][0] = newBoarder[i];
   }
 }
 
-inline void Grid::SetRightBoarder(const std::vector<double> &newBoarder) {
+inline void Grid::SetRightBoarder(const line_t &newBoarder) {
   for (int i = 0; i < _M; ++i) {
     _nodes[i][_N - 1] = newBoarder[i];
   }
 }
 
-inline std::vector<double> Grid::GetColumn(int m) { return _nodes[m]; }
+inline Grid::line_t Grid::GetColumn(int m) { return _nodes[m]; }
 
-inline std::vector<double> Grid::GetRow(int n) {
-  std::vector<double> res(_M);
+inline Grid::line_t Grid::GetRow(int n) {
+  line_t res(_M);
   for (int i = 0; i < _M; ++i) {
     res[i] = _nodes[i][n];
   }
